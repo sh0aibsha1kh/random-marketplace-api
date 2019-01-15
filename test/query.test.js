9ee2845ae2178e5f5b829a6bff56bc7235b11814
Shoaib Shaikh <shoaibmuhammedshaikh@gmail.com>
2019-01-15 05:34:24 -0500

    Add tests

---
+++ test/query.test.js
@@ -0,0 +1,23 @@
+const { request } = require('graphql-request');
+
+const host = "http://localhost:4000";
+const query = `
+query {
+    getProductByTitle(title: "pants") {
+        title
+    }
+}`;
+
+const expected = {
+    "getProductByTitle": [
+      {
+        "title": "pants"
+      }
+    ]
+  }
+
+test('Request pants', async () => {
+    const response = await request(host, query);
+    expect(response).toEqual(expected);
+});
+
